bb8793648b2a4e1c83cba3650556dd39a9479c21
Kyle Edwards <businesskylepedwards@outlook.com>
2024-06-07 09:57:55 -0500

    Project Completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FizzBuzz
+
+['FizzBuzz'](https://www.tomdalling.com/blog/software-design/fizzbuzz-in-too-much-detail/) is a challenge used by recruiters in job interviews to test and see if the applicant can actually code.
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,48 @@
 import os
+
+
+def clear_screen():
+    return os.system("clear || cls")
+
+
+def menu():
+    clear_screen()
+    print("\nWelcome to FizzBuzz. All numbers are between 1 and 100.")
+
+    user_options = {
+        'fizz': [number for number in range(1,101) if number % 3 == 0],
+        'buzz': [number for number in range(1,101) if number % 5 == 0],
+        'fizzbuzz': [number for number in range(1,101) if number % 3 == 0 and number % 5 == 0]
+    }
+
+    while True:
+        user_input = input(
+            "\nWhat would you like to do?"
+            "\n- Enter 'fizz' to see numbers divisible by 3"
+            "\n- Enter 'buzz' to see numbers divisible by 5"
+            "\n- Enter 'fizzbuzz' to see numbers divisible by 3 AND 5"
+            "\n- Enter 'clear' to clear the terminal screen"
+            "\n- Enter 'quit' to exit the program: "
+        ).lower()
+    
+        if user_input == 'fizz':
+            clear_screen()
+            print(f"Printing numbers from 1 to 100 that are divisible by 3:\n{user_options['fizz']}")
+        elif user_input == 'buzz':
+            clear_screen()
+            print(f"Printing numbers from 1 to 100 that are divisible by 5:\n{user_options['buzz']}")
+        elif user_input == 'fizzbuzz':
+            clear_screen()
+            print(f"Printing numbers from 1 to 100 that are divisible by 3 AND 5:\n{user_options['fizzbuzz']}")
+        elif user_input == 'clear':
+            clear_screen()
+        elif user_input == 'quit':
+            clear_screen()
+            print("\nThanks for using FizzBuzz. Have a good day.\n")
+            break
+        else:
+            clear_screen()
+            print("\nUnknown input. Try again.")
+
+
+menu()
